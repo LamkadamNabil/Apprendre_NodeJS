12593e02ddd1b583e8e064df899d1ba33c584b1d
LamkadamNabil <na.lamkadam@gmailcom>
2022-03-26 00:21:42 +0100

    shared doc npm and model

--- Module/calc.js
+++ Module/calc.js
@@ -4,7 +4,5 @@ function add(a,b){
 function sub(a,b){
     console.log('a+b',a+b)
 }
-
-
 exports.MyExp_add=add ;
 
---
+++ api/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+
+
+///
+
+const port = process.env.PORT ||3000
+
+////
+app.use(express.json())
+app.use(express.urlencoded())
+
+
+
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
